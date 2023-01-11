10e54a76bcf51aa625e7775fb47d9cd9a880e8b8
FragoLanten <rizinvadim@gmail.com>
2023-01-11 22:41:49 +0300

    Простое CRUD приложение с использованием технологий Spring, Hibernate, MVC. Присутствуют функции добавления нового работника, изменение работника и удаление работника. Стартовая страница отображает список всех работников.

---
+++ src/main/Java/com/mvc/dao/EmployeeDao.java
@@ -0,0 +1,17 @@
+package com.mvc.dao;
+
+import com.mvc.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee (Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
